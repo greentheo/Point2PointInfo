91e0432865a65bd986202809e730a0cfe3b4b7fe
Kent Hurd <kenth3@gmail.com>
2014-12-23 22:34:40 -0700

    moving files around so the Grunt tasks won't be as onerous.  hoping I don't regret this later

--- mobileApp/Gruntfile.js
+++ mobileApp/Gruntfile.js
@@ -23,12 +23,47 @@ module.exports = function(grunt) {
                     minify: true
                 }
             }
+        },
+        copy: {
+            dev: {
+                files: [
+                    {   // everything but js, since we'll deal with that separately
+                        expand: true,
+                        src: [ 'app/**', '!/app/**/*.js'],
+                        dest: 'dev/'
+                    }
+                ],
+                options: {
+                    process: function (content, srcpath) {
+                        if (srcpath == 'app/index.html') {
+                            return replaceIndexPaths(content, 'dev');
+                        }
+                    }
+                }
+            },
+            prod: {
+                files: [
+                    {   // everything but js, since we'll deal with that separately
+                        expand: true,
+                        src: [ 'app/**', '!/app/**/*.js'],
+                        dest: 'www/'
+                    }
+                ]
+            }
         }
 
     });
 
+    var replaceIndexPaths = function(content, parentDest) {
+
+        // lib stuff
+
+        content.replace(/data-main="main"/g, 'data-main')
+    };
+
     // Load the plugin that provides the "durandaljs" task.
     grunt.loadNpmTasks('grunt-durandaljs');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
     // Default task(s).
     grunt.registerTask('default', ['durandaljs']);
