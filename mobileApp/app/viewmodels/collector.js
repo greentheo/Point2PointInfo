50a3a48f04d3c325310c9978005d74f0dd37572d
Kent Hurd <kenth3@gmail.com>
2014-12-30 12:59:15 -0700

    tweaks

--- mobileApp/app/viewmodels/collector.js
+++ mobileApp/app/viewmodels/collector.js
@@ -8,12 +8,9 @@
 function(app, appData, deviceAcceleration, deviceLocation, observables) {
     var vm = {
         userName: '',
-        interval: 3000,
         buttonCaption: 'Start',
-        collectionHandle: null,
         collectionStart: null,
         collectionEnd: null,
-        accelerometerHandle: null,
         locationEntryCount: 0,
         locationErrorCount: 0,
         accelerometerEntryCount: 0,
@@ -38,27 +35,27 @@ function(app, appData, deviceAcceleration, deviceLocation, observables) {
 
             deviceLocation.start();
             app.on('location.start').then(function() {
-
+                // TODO: do we need this?
             });
 
             app.on('location.capture').then(function(data) {
-
+                that.locationEntryCount = appData.locationData.length;
             });
 
             app.on('location.unsupported').then(function() {
 
             });
 
-
             // acceleration
+
             deviceAcceleration.start();
 
             app.on('acceleration.start').then(function() {
-
+                // TODO: do we need this?
             });
 
             app.on('acceleration.capture').then(function(data) {
-
+                that.locationEntryCount = appData.accelerationData.length;
             });
 
             app.on('acceleration.unsupported').then(function() {
@@ -85,7 +82,7 @@ function(app, appData, deviceAcceleration, deviceLocation, observables) {
     // note: one disadvantage of using the observable plugin is that the syntax for creating
     //      what would normally be a ko computed function is a little ... um ... different
     observables.defineProperty(vm, 'canCollect', function() {
-        return this.userName != '' && this.interval > 0;
+        return this.userName != '';
     });
 
     return vm;
