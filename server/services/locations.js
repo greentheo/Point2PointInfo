264ce762c0d1963093baea23450252268ad572e9
Kent Hurd <kenth3@gmail.com>
2015-01-11 13:09:43 -0700

    consolidated some Mongoose schema code

--- server/services/locations.js
+++ server/services/locations.js
@@ -1,5 +1,25 @@
 var Mongoose = require("mongoose");
-var Location = require("../schema/locationschema").Location;
-var connection = Mongoose.connect("mongodb://localhost/bottles");
+var db = Mongoose.connect("mongodb://localhost/bottles");
+
+var Location = Mongoose.model("Location", {
+    userId: { type: Number, required: true },
+    location: {
+        timestamp: { type: Date, required: true },
+        coords: {
+            latitude: { type: Number, required: true },
+            longitude: { type: Number, required: true },
+            altitude: { type: Number },                     // in meters
+            accuracy: { type: Number },                     // (of latitude and longitude in meters) - Android not supported
+            altitudeAccuracy: { type: Number },
+            heading: { type: Number  },                     // (degrees clockwise relative to true north)
+            speed: { type: Number }                         // (meters per second)
+        },
+        accelerometer: [{
+            x: { type: Number, required: true },
+            y: { type: Number, required: true },
+            z: { type: Number, required: true }
+        }]
+    }
+});
 
 // now what?
--- server/services/users.js
+++ server/services/users.js
@@ -1,5 +1,17 @@
-var Mongoose = require("mongoose");
-var User = require("../schema/userschema").User;
-var connection = Mongoose.connect("mongodb://localhost/bottles");
+var Mongoose = require('mongoose');
+var db = Mongoose.connect('mongodb://localhost/bottles');
+
+var User = Mongoose.model("User", {
+    login: { type: String, required: true },
+    password: { type: String, required: true },
+    fullName: { type: String, required: true },
+    roles: [{ name: { type: String, required: true } }]
+});
 
 // now what?
+db.once('open', function(cb) {
+
+
+
+
+});
