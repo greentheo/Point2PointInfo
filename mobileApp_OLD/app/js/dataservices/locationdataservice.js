9abefb2760b92360632d91bd982790ae6b00ccdf
Kent Hurd <kenth3@gmail.com>
2015-06-21 17:56:56 -0600

    removed old mobileApp folder

--- mobileApp_OLD/app/js/dataservices/locationdataservice.js
+++
@@ -1,7 +0,0 @@
-define(['js/dataservices/basedataservice'], function(svc) {
-    return {
-        postLocationData: function(locationData, success, fail) {
-            return svc.postJson('/location/savelocationdata', locationData, success, fail);
-        }
-    };
-});
--- mobileApp_OLD/app/js/utils.js
+++
@@ -1,8 +0,0 @@
-define(function() {
-    return {
-        roundNumber: function (number, places) {
-            var rounded = +(Math.round(number + "e+" + places) + "e-" + places);
-            return rounded;
-        }
-    };
-});
